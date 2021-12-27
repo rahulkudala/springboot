0c27e198e1bb13427e59597d7b92abbfc4c8ef76
Rahul K <rahul.kudala@cjsstechnologies.com>
2021-12-27 10:59:09 +0530

    adding controller

---
+++ Springs/springboot_project/src/main/java/com/example/springboot_project/Controller.java
@@ -0,0 +1,25 @@
+package com.example.springboot_project;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/page")
+    public String page(){
+        String a = "Welcome to ";
+        String b = "Page Method";
+        
+        return a+b;
+    }
+    @GetMapping("/num")
+    public int page2(){
+
+        return 10*20;
+    }
+
+}
